6610c9992901faa010304af3e405053f6c848c8e
antoinenry <antoinenry@gmail.com>
2023-12-28 11:59:02 -0500

    minor code rewrite

--- Assets/Scripts/Editor/GameStateInspector.cs
+++ Assets/Scripts/Editor/GameStateInspector.cs
@@ -4,24 +4,28 @@ using UnityEngine;
 [CustomEditor(typeof(GameState))]
 public class GameStateInspector : Editor
 {
+    GameState state;
+
+    private void OnEnable()
+    {
+        state = target as GameState;
+    }
+
     public override void OnInspectorGUI()
     {
         base.OnInspectorGUI();
         EditorGUILayout.BeginHorizontal("box");
         if (GUILayout.Button("Reset scores"))
         {
-            GameState state = target as GameState;
             state.ClearCurrentScore();
             state.ClearHighscores();
         }
         if (GUILayout.Button("Save"))
         {
-            GameState state = target as GameState;
             state.SaveState();
         }
         if (GUILayout.Button("Load"))
         {
-            GameState state = target as GameState;
             state.TryLoadState();
         }
         EditorGUILayout.EndHorizontal();
